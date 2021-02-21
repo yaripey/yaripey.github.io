b864c3dde79172648ea33312dcc5139b778f6a7b
yaripey <17trubin@gmail.com>
2021-02-21 19:16:11 +0200

    Added some styles and a mobile header

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileHeaderButton = document.querySelector('.mobile-header-button')
+  const mobileHeader = document.querySelector('.header-right')
+  const logo = document.querySelector('.logo')
+
+  mobileHeaderButton.addEventListener('click', () => {
+    mobileHeader.classList.toggle('opened-header')
+    logo.classList.toggle('closed-logo')
+  })
+})
